9454ab99cb595e222b823bbd9b877aaabe52ac3e
Nick Rameau <r@meau.me>
2018-11-06 13:51:01 -0500

    [WIP] Initial code for flow rate gathering

--- report_server/routes/getIotileData.js
+++ report_server/routes/getIotileData.js
@@ -3,17 +3,48 @@ const router = express.Router();
 const iotileBaseUrl = 'https://iotile.cloud/api/v1';
 const axios = require('axios');
 
-/* GET users listing. */
 router.get('/', async (req, res) => {
-    const [err, user] = await __hp(getUserInfo());
+    const [err, latestFeedFlowmeterEntry] = await __hp(getLatestFeedFlowmeterEntry());
+
     if (err) {
         console.log(err);
         return __te(err, res, 500, {});
     }
     return res.json({
-        message: user
+        message: latestFeedFlowmeterEntry
     })
 });
+
+// This is a hack for now, needs to figure out the values for these IDs
+const latestFeedFlowmeterEntryQuery = `SELECT 
+    *
+FROM
+    reading
+WHERE
+    kiosk_id = 1 AND parameter_id = 13
+        AND sampling_site_id = 2
+ORDER BY id DESC
+LIMIT 1;
+`;
+
+// We need the latest entry so we know where to continue gathering
+// data from
+const getLatestFeedFlowmeterEntry = () => {
+    return new Promise((resolve, reject) => {
+        __pool.getConnection(async (err, connection) => {
+            connection.query(latestFeedFlowmeterEntryQuery, (err, result) => {
+                if (!err) {
+                    if (Array.isArray(result) && result.length > 0) {
+                        return resolve(result);
+                    } else {
+                        return resolve();
+                    }
+                }
+                return reject(err);
+            });
+        });
+    });
+}
 
 const getUserInfo = () => {
     return new Promise((resolve, reject) => {

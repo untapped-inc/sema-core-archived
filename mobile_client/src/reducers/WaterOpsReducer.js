ad91234e76fcdc4ae11075e2933624235fef746c
Nick Rameau <r@meau.me>
2018-12-04 14:31:09 -0500

    Create water ops reducer

---
+++ mobile_client/src/reducers/WaterOpsReducer.js
@@ -0,0 +1,26 @@
+import { SET_WATER_OP_CONFIGS } from "../actions/WaterOpActions"
+
+let initialState = {
+    configs: {
+        idMapping: null,
+        parameters: null,
+        samplingSites: null
+    }
+};
+
+const waterOpsReducer = (state = initialState, action) => {
+	console.log(`waterOpsReducer: ${action.type}`);
+    switch (action.type) {
+        case SET_WATER_OP_CONFIGS:
+            const configs = {...action.data}
+            return {
+                ...state,
+                configs
+            }
+
+		default:
+			return state;
+	}
+};
+
+export default waterOpsReducer;

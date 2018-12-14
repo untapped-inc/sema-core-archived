e41cf8e0c0d8d1a76487e1965a7d232a7a03ae14
Nick Rameau <r@meau.me>
2018-12-14 03:41:11 -0500

    Only save the final mapping

--- mobile_client/src/reducers/WaterOpsReducer.js
+++ mobile_client/src/reducers/WaterOpsReducer.js
@@ -2,9 +2,7 @@ import { SET_WATER_OP_CONFIGS } from "../actions/WaterOpActions"
 
 let initialState = {
     configs: {
-        idMapping: null,
-        parameters: null,
-        samplingSites: null
+        samplingSiteParameterMapping: null
     }
 };
 
@@ -12,7 +10,10 @@ const waterOpsReducer = (state = initialState, action) => {
 	console.log(`waterOpsReducer: ${action.type}`);
     switch (action.type) {
         case SET_WATER_OP_CONFIGS:
-            const configs = {...action.data}
+            const configs = {
+                samplingSiteParameterMapping: action.data
+            };
+            
             return {
                 ...state,
                 configs

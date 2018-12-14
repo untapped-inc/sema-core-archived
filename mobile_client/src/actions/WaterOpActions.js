a564971f9e2b9b19a0ffd44d5e12e57bef0d6937
Nick Rameau <r@meau.me>
2018-12-14 03:36:21 -0500

    Refactor commented code and send a prettier mapping list

--- mobile_client/src/actions/WaterOpActions.js
+++ mobile_client/src/actions/WaterOpActions.js
@@ -1,11 +1,10 @@
+import Events from "react-native-simple-events";
+
 export const SET_WATER_OP_CONFIGS = 'SET_WATER_OP_CONFIGS';
 
-export function setWaterOpConfigs(waterOpConfigs) {
-	console.log("setWaterOpConfigs - action");
+// Creates and returns an array looking like this
 
-	// Create an array looking like this
-
-	// const mapping = [
+	// const samplingSiteParameterMapping = [
 	// 	{
 	// 		name: 'A:Feed',
 	//		id: 2,
@@ -20,51 +19,59 @@ export function setWaterOpConfigs(waterOpConfigs) {
 	// 		]
 	// 	}
 	// ]
+export function setWaterOpConfigs(waterOpConfigs) {
+	console.log("setWaterOpConfigs - action");
 
 	const samplingSites = waterOpConfigs.samplingSites;
 	const parameters = waterOpConfigs.parameters;
 	const idMapping = waterOpConfigs.samplingSiteParameterMapping;
 
-	// const samplingSiteParameterMapping = idMapping
-	// 	.reduce((final, next) => {
-	// 		let currentSamplingSite = samplingSites.reduce((samplingSite, nextSamplingSite) => {
-	// 			if (nextSamplingSite.id === next.sampling_site_id) return nextSamplingSite;
-	// 		}, null);
-	// 		// get current parameter
-	// 		let currentParameter = parameters.reduce((parameter, nextParameter) => {
-	// 			if(nextParameter.id === next.parameter_id) return nextParameter;
-	// 		}, null);
+	const samplingSiteParameterMapping = idMapping
+		.reduce((finalMapping, next) => {
+			// get current sampling site
+			let currentSamplingSite = samplingSites.reduce((samplingSite, nextSamplingSite) => {
+				if (nextSamplingSite.id === next.sampling_site_id) return nextSamplingSite;
+				return samplingSite;
+			}, null);
+			
+			// get current parameter
+			let currentParameter = parameters.reduce((parameter, nextParameter) => {
+				if(nextParameter.id === next.parameter_id) return nextParameter;
+				return parameter;
+			}, null);
 
-	// 		// check if samplingSite is already in final
-	// 		let currentMappingIndex = final.reduce((mapping, next, index) => {
-	// 			if (next.id === currentSamplingSite.id) return index;
-	// 		}, -1);
+			// check if samplingSite is already in finalMapping
+			let currentMappingIndex = finalMapping.reduce((mapping, next, index) => {
+				if (next.id === currentSamplingSite.id) return index;
+				return mapping;
+			}, -1);
 
-	// 		// If it is in final, push current parameter into its parameters array
-	// 		if (currentMappingIndex !== -1 && final[currentMappingIndex]) {
-	// 			final[currentMappingIndex].parameters.push(currentParameter);
-	// 		} else { 
-	// 			// If it's not in final, create a new object for this sampling site
-	// 			// looking like this
-	// 			// const newSamplingSite = {
-	// 			// 	name: samplingSite.name,
-	// 			// 	id: samplingSite.id,
-	// 			// 	parameters: [{...currentParameter}]
-	// 			// }
-	// 			final.push({
-	// 				name: currentSamplingSite.name,
-	// 				id: currentSamplingSite.id,
-	// 				parameters: [{...currentParameter}]
-	// 			});
-	// 		}
-	// 		return final;
-	// 	}, []);
+			// If it is in finalMapping, push current parameter into its parameters array
+			if (currentMappingIndex !== -1 && finalMapping[currentMappingIndex]) {
+				finalMapping[currentMappingIndex].parameters.push(currentParameter);
+			} else { 
+				// If it's not in finalMapping, create a new object for this sampling site
+				finalMapping.push({
+					name: currentSamplingSite.name,
+					id: currentSamplingSite.id,
+					parameters: [{...currentParameter}]
+				});
+			}
+			return finalMapping;
+		}, []);
 
-	const finalData = {
-		samplingSites,
-		parameters,
-		idMapping
-	}
+	// Sort sampling sites alphabetically
+	samplingSiteParameterMapping.sort((a, b) => {
+		if (a.name < b.name) return -1;
+		return 1;
+	})
 
-	return (dispatch) => { dispatch({ type: SET_WATER_OP_CONFIGS, data: finalData }); };
+	Events.trigger('WaterOpsConfigReady', samplingSiteParameterMapping[0]);
+
+	return (dispatch) => {
+		dispatch({
+			type: SET_WATER_OP_CONFIGS,
+			data: samplingSiteParameterMapping
+		});
+	};
 }

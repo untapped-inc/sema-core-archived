3810a3926d1a50120f89b764bb48e1a8fcafaf57
Nick Rameau <r@meau.me>
2018-12-27 15:57:40 -0500

    [Refactor] Keeping it simple

--- mobile_client/src/components/water-ops/FeedParameters.js
+++ mobile_client/src/components/water-ops/FeedParameters.js
@@ -1,14 +1,9 @@
 import React, { Component } from "react";
 import {
     StyleSheet,
-    View,
-    Text,
-    Image,
-    TouchableHighlight,
-    Modal,
-    FlatList,
     ScrollView,
-    TextInput
+    TextInput,
+    Text
 } from 'react-native';
 
 import { bindActionCreators } from "redux";
@@ -21,49 +16,94 @@ import i18n from '../../app/i18n';
 class FeedParameters extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            tableHead: ['', 'Value', 'Unit'],
-            tableData: null,
-            temperature: null
-        };
-
-        this.parameterRows = [
-            <Text style={styles.parameter_name}>Temperature</Text>,
-            <View>
-                <TextInput
-                    style={styles.table_input}
-                    underlineColorAndroid="transparent"
-                    placeholder="10 - 30"
-                    keyboardType="numeric"
-                    value={this.state.temperature}
-                    onChangeText={temperature => {this.setState({temperature})}} />
-            </View>,
-            <Text style={styles.parameter_unit}>0C</Text>
-        ];
     }
 
     render() {
         return (
-            <View style={styles.table_container}>
+            <ScrollView style={styles.dataWrapper}>
                 <Table borderStyle={{ borderWidth: 0 }}>
-                    <Row data={this.state.tableHead} style={styles.table_header} textStyle={styles.table_header_text} />
+                    <Row
+                        data={[
+                            <Text style={styles.parameter_name}>Temperature</Text>,
+                            <TextInput
+                                ref={ref => {
+                                    this.temperature = ref;
+                                }}
+                                style={styles.table_input}
+                                underlineColorAndroid="transparent"
+                                placeholder='10 - 30'
+                                keyboardType="numeric"/>,
+                            <Text style={styles.parameter_unit}>°C</Text>
+                        ]}
+                        style={[styles.table_row, { backgroundColor: '#F0F8FF' }]}
+                        textStyle={styles.table_row_text}
+                    />
+                    <Row
+                        data={[
+                            <Text style={styles.parameter_name}>pH</Text>,
+                            <TextInput
+                                ref={ref => {
+                                    this.ph = ref;
+                                }}
+                                style={styles.table_input}
+                                underlineColorAndroid="transparent"
+                                placeholder='5 - 9'
+                                keyboardType="numeric"/>,
+                            <Text style={styles.parameter_unit}>-</Text>
+                        ]}
+                        style={[styles.table_row]}
+                        textStyle={styles.table_row_text}
+                    />
+                    <Row
+                        data={[
+                            <Text style={styles.parameter_name}>Total Alkalinity</Text>,
+                            <TextInput
+                                ref={ref => {
+                                    this.totalAlkalinity = ref;
+                                }}
+                                style={styles.table_input}
+                                underlineColorAndroid="transparent"
+                                placeholder='1 - 800'
+                                keyboardType="numeric"/>,
+                            <Text style={styles.parameter_unit}>mg/L CaCO3 (ppm)</Text>
+                        ]}
+                        style={[styles.table_row, { backgroundColor: '#F0F8FF' }]}
+                        textStyle={styles.table_row_text}
+                    />
+                    <Row
+                        data={[
+                            <Text style={styles.parameter_name}>Total Hardness</Text>,
+                            <TextInput
+                                ref={ref => {
+                                    this.totalHardness = ref;
+                                }}
+                                style={styles.table_input}
+                                underlineColorAndroid="transparent"
+                                placeholder='1 - 800'
+                                keyboardType="numeric"/>,
+                            <Text style={styles.parameter_unit}>mg/L CaCO3 (ppm)</Text>
+                        ]}
+                        style={[styles.table_row]}
+                        textStyle={styles.table_row_text}
+                    />
+                    <Row
+                        data={[
+                            <Text style={styles.parameter_name}>Total Dissolved Solids</Text>,
+                            <TextInput
+                                ref={ref => {
+                                    this.totalDissolvedSolids = ref;
+                                }}
+                                style={styles.table_input}
+                                underlineColorAndroid="transparent"
+                                placeholder='0 - 1300'
+                                keyboardType="numeric"/>,
+                            <Text style={styles.parameter_unit}>mg/L (ppm)</Text>
+                        ]}
+                        style={[styles.table_row, { backgroundColor: '#F0F8FF' }]}
+                        textStyle={styles.table_row_text}
+                    />
                 </Table>
-                <ScrollView style={styles.dataWrapper}>
-                    <Table borderStyle={{ borderWidth: 0 }}>
-                        {
-                            parameterRows.map((rowData, index) => (
-                                <Row
-                                    key={index}
-                                    data={rowData}
-                                    style={[styles.table_row, index % 2 && { backgroundColor: '#F0F8FF' }]}
-                                    textStyle={styles.table_row_text}
-                                />
-                            ))
-                        }
-                    </Table>
-                </ScrollView>
-            </View>
+            </ScrollView>
         );
     }
 }
@@ -81,7 +121,7 @@ function mapDispatchToProps(dispatch) {
     };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Toolbar);
+export default connect(mapStateToProps, mapDispatchToProps)(FeedParameters);
 
 const styles = StyleSheet.create({
     parameter_name: {
@@ -96,21 +136,6 @@ const styles = StyleSheet.create({
     table_input: {
         borderBottomWidth: 1,
         borderBottomColor: '#2858a7'
-    },
-
-    table_container: {
-        flex: 1,
-        backgroundColor: '#fff'
-    },
-
-    table_header: {
-        height: 40,
-        backgroundColor: '#ABC1DE'
-    },
-
-    table_header_text: {
-        fontWeight: 'bold',
-        alignSelf: 'center'
     },
 
     table_row: {

1ad0676d2f53d42c18312883e7b72b2a38b133c9
Nick Rameau <r@meau.me>
2019-01-16 22:29:31 -0500

    [Patch] Name change update

--- mobile_client/ios/SEMAPOSTests/SEMAPOSTests.m
+++ mobile_client/ios/SEMAPOSTests/SEMAPOSTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface SEMAPOSTests : XCTestCase
+@interface semaposTests : XCTestCase
 
 @end
 
-@implementation SEMAPOSTests
+@implementation semaposTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {

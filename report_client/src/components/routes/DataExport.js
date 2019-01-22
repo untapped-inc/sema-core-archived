3019d24413ec923bdeccf21ead2270f3d1fa8c2b
Nick Rameau <r@meau.me>
2019-01-22 16:52:25 -0500

    [Patch] Complete DataExport component

--- report_client/src/components/routes/DataExport.js
+++ report_client/src/components/routes/DataExport.js
@@ -7,13 +7,29 @@ import { bindActionCreators } from "redux";
 import * as healthCheckActions from 'actions/healthCheckActions';
 import { withRouter } from 'react-router';
 import '../../css/DataExport.css';
-import LoadProgress from "../LoadProgress";
+import '../../css/glyphicon-spinner.css';
+import { axiosService } from 'services';
+import { CSVLink } from 'react-csv';
+import { ToastContainer, ToastStore } from 'react-toasts';
 
 import $ from 'jquery';
 import 'selectize';
 import '../../../node_modules/selectize/dist/css/selectize.css';
 
 class DataExport extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			dataset: 'sales',
+			data: ''
+		}
+
+		this.onBeginDownload = this.onBeginDownload.bind(this);
+		this.onDatasetChange = this.onDatasetChange.bind(this);
+
+		this.csvLink = React.createRef();
+	}
 	render() {
 		return this.showContent();
 	}
@@ -29,23 +45,85 @@ class DataExport extends Component {
 	}
 
 	componentDidMount() {
-		$('select').selectize();
+		$('select').selectize({
+			onChange: value => {
+				this.onDatasetChange(value);
+			}
+		});
+	}
+
+	onDatasetChange(value) {
+		this.setState({
+			dataset: value
+		});
+	}
+
+	onBeginDownload() {
+		this.setState({
+			loading: true
+		});
+
+		axiosService
+			.get('/sema/data-export/', {
+				params: {
+					dataset: this.state.dataset
+				}
+			})
+			.then(response => {
+				this.setState({
+					loading: false
+				});
+				if (response.status === 200) {
+					if (!response.data || !response.data.length) {
+						if (this.state.dataset === 'sales') {
+							return ToastStore.info("No sales data for this year");
+						} else {
+							return ToastStore.info("No water quality data for this year");
+						}
+					}
+					this.setState({
+						data: response.data
+					}, () => {
+						this.csvLink.current.link.click();
+					});
+				} else {
+					return [];
+				}
+			})
+			.catch(function (error) {
+				console.log(error);
+				return error;
+			});
 	}
 
 	showDataExport() {
 		return (
 			<div className="DataExportContainer" style={this.getHeight()}>
 				<div className="DataExportContent">
-					<div class="well well-lg DataExportWell">
+					<div className="well well-lg DataExportWell">
 						<select
 							id="select-dataset"
-							class="selectized"
+							className="selectized"
 							placeholder="Select a dataset">
-								<option value="1" selected>Year to Date Sales Data</option>
-								<option value="2">Year to Date Water Quality Data</option>
+							<option value="sales">Year to Date Sales Data for All Sites</option>
+							<option value="water-quality">Year to Date Water Quality Data for All Sites</option>
 						</select>
+						<button className="btn btn-lg btn-block DataExportButton" onClick={this.onBeginDownload}>
+							<div className='ButtonIcon'>
+								<i className={`glyphicon ${this.state.loading ? 'glyphicon-refresh fast-right-spinner' : 'glyphicon-download'}`}></i>
+							</div>
+							Download Now
+						</button>
+						<CSVLink
+							data={this.state.data}
+							filename={`YTD-${this.state.dataset}-data.csv`}
+							className="hidden"
+							ref={this.csvLink}
+							target="_blank"
+						/>
 					</div>
 				</div>
+				<ToastContainer position={ToastContainer.POSITION.BOTTOM_CENTER} store={ToastStore} />
 			</div>
 		);
 	}
